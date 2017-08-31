c8017de75b5fe66d72b607bae8a3c814e6d4b6b8
Ali <30610753+aamining@users.noreply.github.com>
2017-08-31 14:36:44 +1000

    change in routs

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   # redirect when go to localhost:3000
-  root to: 'pages#help'
+  root to: 'pages#main'
 
   # redirect when go to localhost:3000/pages/help
-  get 'pages/help', to: 'pages#help'
+  get 'pages/main', to: 'pages#main'
 
   #or details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
